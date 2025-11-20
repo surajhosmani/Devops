098043011e8b628e807c8399fdd63598a2731e0e
surajhosmani <surajhosmani83@gmail.com>
2025-11-20 14:23:54 +0530

    Create std.py

---
+++ std.py
@@ -0,0 +1,9 @@
+import sys
+if len(sys.argv) !- 3:
+   print("Usage python student.py <name> <roll sys.exit(1)
+   sys.exit(1)
+script_name = sys.argv[0]
+name = sys.argv[2]
+print("Script name:",script,script_name)
+print("Student Name",name)
+print("Roll number:",rollno)
