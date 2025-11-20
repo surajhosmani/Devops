c3a7ef32cf0ed82e5cbe4ab05dfcf1b3f0ff41e6
surajhosmani <surajhosmani83@gmail.com>
2025-11-20 14:39:56 +0530

    std.py

--- std.py
+++ std.py
@@ -1,6 +1,6 @@
 import sys
 if len(sys.argv) !- 3:
-   print("Usage python student.py <name> <roll sys.exit(1)
+   print("Usage python student.py <name> <rollno> sys.exit(1)
    sys.exit(1)
 script_name = sys.argv[0]
 name = sys.argv[2]
